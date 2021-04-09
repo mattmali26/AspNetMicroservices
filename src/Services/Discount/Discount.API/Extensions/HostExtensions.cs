be3fb98ccade36655e7ace244dc43ac68c2b8f6b
mattmali26 <mattia.malica@gmail.com>
2021-04-09 10:58:29 +0200

    Fix discount grpc url in docker

--- src/Services/Discount/Discount.API/Extensions/HostExtensions.cs
+++ src/Services/Discount/Discount.API/Extensions/HostExtensions.cs
@@ -35,7 +35,7 @@ namespace Discount.API.Extensions
                     command.ExecuteNonQuery();
 
                     command.CommandText = @"CREATE TABLE Coupon(Id SERIAL PRIMARY KEY,
-                                                                ProductName VAECHAR(24) NOT NULL,
+                                                                ProductName VARCHAR(24) NOT NULL,
                                                                 Description TEXT,
                                                                 Amount INT)";
                     command.ExecuteNonQuery();
